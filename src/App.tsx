5422acdd470d0bbd5330a219a66f48001d2cc5df
Darvin <darvingaba1@gmail.com>
2022-08-01 09:47:19 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   
   function getItemImagePath(item: Item) {
     let id = String(item.id).padStart(3, "0");
-    return `public/assets/icons/${id}-${item.name}.svg`;
+    return `/assets/icons/${id}-${item.name}.svg`;
   }
 
   
--- src/components/HeaderList.tsx
+++ src/components/HeaderList.tsx
@@ -1,11 +1,23 @@
 import {HeaderListItem} from "./HeaderItem";
 
-export function HeaderList({items, increaseQuantity, getItemImagePath}) {
+type Item = {
+  id: number;
+  name: string;
+  price: number;
+  stock: number;
+  inCart: number;
+};
+type Props = {
+    items: Item[];
+    increaseQuantity: Function;
+    getItemImagePath: Function;
+}
+export function HeaderList({items, increaseQuantity, getItemImagePath}: Props) {
     return (
       <ul className="item-list store--item-list">
         {items.map((items) => (
           <HeaderListItem
-            // key={item.id}
+            key={items.id}
             item={items}
             increaseQuantity={increaseQuantity}
             getItemImagePath={getItemImagePath}
